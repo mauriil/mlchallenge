bee7f56f76f6b54cf85c9ec1057a0168a8bcf1cd
Mauricio Lahitte <Mauricio_Lahitte@intranet.itresources>
2021-07-22 09:20:58 -0300

    added endpoint to retrieve mails

--- middlewares/express.js
+++ middlewares/express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const APP = express();
+const consultModel = require('../models/consult');
 
 APP.get('/', (req, res) => {
     res.send('Hello World!')
@@ -7,6 +8,17 @@ APP.get('/', (req, res) => {
 
 APP.get('/newcode', (req, res) => {
     res.status(200).json({"status": "Application code found, copy and paste in the script console", "applicationCode": req.query.code})
+  });
+
+APP.get('/mails/:guestMail', (req, res) => {
+  consultModel.findAll({
+    where: {
+      guestMail: req.params.guestMail,
+    }
+  }).then(result => {
+    res.status(200).json(result);
+  });
+    
   });
   
 module.exports = APP;
